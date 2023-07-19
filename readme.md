c291bee2d2a274e00281a3719e70137522cedc0f
NadiaOl <115547708+NadiaOl@users.noreply.github.com>
2023-07-19 17:25:29 +0300

    HW3

--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
 ## GoIT Node.js Course Template Homework
 
+mongodb+srv://NadiaOl:GjkVfh111@cluster0.czygkg6.mongodb.net/
+
 Виконайте форк цього репозиторію для виконання домашніх завдань (2-6)
 Форк створить репозиторій на вашому http://github.com
 
--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 import app from "./app.js";
 
-app.listen(3004, () => {
-  console.log("Server running. Use our API on port: 3004")
+import mongoose from 'mongoose';
+const DB_HOST = "mongodb+srv://NadiaOl:GjkVfh111@cluster0.czygkg6.mongodb.net/contacts_reader?retryWrites=true&w=majority";
+
+mongoose.set('strictQuery', true)
+
+mongoose.connect(DB_HOST)
+.then(() => { 
+  app.listen(3000)
+  console.log("Database connection successful")})
+.catch(error => {
+  console.log(error.message);
+  process.exit(1);
 })
+
